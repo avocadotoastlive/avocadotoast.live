0a880cf3b84ba752f844ba89029b578996aabc43
Cat Chen <catchen@catchen.me>
2019-12-13 23:55:29 -0800

    Initialized Amplitude in episode pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.setTemplateFormats("liquid");
-  
+
+  eleventyConfig.addFilter("stringify", function(value) {
+    return JSON.stringify(value);
+  });
+
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
