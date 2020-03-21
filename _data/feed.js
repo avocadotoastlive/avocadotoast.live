6c7484d451e144de64cfffb5299d63e80bd88dfc
Cat Chen <catchen@catchen.me>
2020-03-21 16:11:44 -0700

    Switched data source from GraphQL to multiple feeds

--- _data/feed.js
+++
@@ -1,9 +0,0 @@
-const Parser = require('rss-parser');
-
-const RSS_URL = 'https://anchor.fm/s/e3e1fd0/podcast/rss';
-
-module.exports = async function() {
-  const parser = new Parser();
-  const result = await parser.parseURL(RSS_URL);
-  return result;
-};
