79d8d0cec9e08367d9946c869d17ff6973dbc458
Cat Chen <catchen@catchen.me>
2020-03-23 22:52:54 -0700

    Logging source error

--- js/site.js
+++ js/site.js
@@ -4,54 +4,66 @@ $().ready(function() {
   var episodeMatches = window.location.pathname.match(/\/episodes\/(\d+)/);
   var episode = episodeMatches ? 'episode:' + episodeMatches[1] : 'home';
 
-  function logEvent(category, action, label, value, interaction) {
+  function logEvent(category, action, label, value, interaction, customDimensions) {
     if (interaction === undefined) {
       interaction = true;
     }
-    window.gtag('event', action, {
+    var dimensions = {
       non_interaction: !interaction,
       event_category: category,
       event_label: label,
-      value: value,
-    });
+      value: value
+    };
+    if (customDimensions !== undefined) {
+      for (var key in customDimensions) {
+        dimensions[key] = customDimensions[key];
+      }
+    }
+    window.gtag('event', action, dimensions);
   }
 
-  logEvent('page', 'view', episode);
+  logEvent('page', 'view');
 
   $('#audioPlayer')
     .on('play', function(event) {
-      logEvent('audio', 'play', episode);
+      logEvent('audio', 'play');
     })
     .on('pause', function(event) {
-      logEvent('audio', 'pause', episode);
+      logEvent('audio', 'pause');
     })
     .on('loadedmetadata', function(event) {
-      logEvent('audio', 'loadedmetadata', episode, undefined, false);
+      logEvent('audio', 'loadedmetadata', undefined, undefined, false);
     })
     .on('loadeddata', function(event) {
-      logEvent('audio', 'loadeddata', episode, undefined, false);
+      logEvent('audio', 'loadeddata', undefined, undefined, false);
     })
     .on('canplay', function(event) {
-      logEvent('audio', 'canplay', episode, undefined, false);
+      logEvent('audio', 'canplay', undefined, undefined, false);
     })
     .on('canplaythrough', function(event) {
-      logEvent('audio', 'canplaythrough', episode, undefined, false);
+      logEvent('audio', 'canplaythrough', undefined, undefined, false);
     })
     .on('waiting', function(event) {
-      logEvent('audio', 'waiting', episode, undefined, false);
+      logEvent('audio', 'waiting', undefined, undefined, false);
     })
     .on('playing', function(event) {
-      logEvent('audio', 'playing', episode, undefined, false);
+      logEvent('audio', 'playing', undefined, undefined, false);
     })
     .on('seeking', function(event) {
-      logEvent('audio', 'seeking', episode, undefined, false);
+      logEvent('audio', 'seeking', undefined, undefined, false);
     })
     .on('seeked', function(event) {
-      logEvent('audio', 'seeked', episode, undefined, false);
+      logEvent('audio', 'seeked', undefined, undefined, false);
     })
     .on('ended', function(event) {
-      logEvent('audio', 'ended', episode, undefined, false);
+      logEvent('audio', 'ended', undefined, undefined, false);
     });
+
+  window.addEventListener('error',  function(event) {
+    logEvent('source', 'error', undefined, undefined, false, {
+      platform: event.target.getAttribute('data-platform')
+    });
+  }, true);
 
   $('.platform-list a')
     .on('click', function(event) {
