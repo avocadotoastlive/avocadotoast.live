5af01ac1892b4fbdea227465bb7728d231f34150
Cat Chen <catchen@catchen.me>
2020-03-28 18:43:13 -0700

    Resize image (#44)
    * Installed sharp for image resizing
    
    * Resizing feed cover photo
    
    * Resizing all images
    
    * Applied resized images to episode list
    
    * Applied resized images to episode page
    
    * Applied resized images to home page
    
    * Changed generated image sizes
    
    * Applied resized images to episode player

--- _data/data.js
+++ _data/data.js
@@ -4,6 +4,7 @@ const FS = { ...require('fs'), ...require('fs').promises };
 const Path = require('path');
 const Parser = require('rss-parser');
 const Axios = require('axios');
+const Sharp = require('sharp');
 
 const ANCHOR_RSS_URL = 'https://anchor.fm/s/e3e1fd0/podcast/rss';
 const TYPLOG_RSS_URL = 'https://avocadotoast.typlog.io/episodes/feed.xml';
@@ -12,6 +13,9 @@ const XIMALAYA_RSS_URL = 'https://www.ximalaya.com/album/29161862.xml';
 // __dirname is the _data directory
 const IMAGE_PATH = '/images/external/';
 const IMAGE_DIRECTORY = Path.resolve(__dirname, '../images/external/');
+
+const IMAGE_SIZES = [1280, 960, 640, 480, 320, 240, 160, 120, 80];
+const IMAGE_TYPES = ['image/webp', 'image/jpeg', 'image/png'];
 
 const fetchFeed = async function(url) {
   const label = `Feed downloaded (${url})`;
@@ -58,7 +62,7 @@ const resetDirectory = async function() {
   console.log(`Created external image directory: ${IMAGE_DIRECTORY}`);
 }
 
-const downloadImage = async function(url, filename) {
+const downloadImage = async function(url, file) {
   const label = `Image downloaded (${url})`;
   console.time(label);
 
@@ -73,17 +77,19 @@ const downloadImage = async function(url, filename) {
   switch (contentType) {
     case 'image/jpg':
     case 'image/jpeg':
-      extension = 'jpg';
+      extension = '.jpg';
       break;
     case 'image/png':
-      extension = 'png';
+      extension = '.png';
       break;
     default:
       console.error(`Unsupported content-type (${contentType}): ${url}`);
       extension = '';
   }
 
-  const path = Path.join(IMAGE_DIRECTORY, `${filename}.${extension}`);
+  const filename = `${file}${extension}`;
+  const path = Path.join(IMAGE_DIRECTORY, filename);
+  const virtualPath = Path.join(IMAGE_PATH, filename);
   const writer = FS.createWriteStream(path);
 
   response.data.pipe(writer);
@@ -91,14 +97,110 @@ const downloadImage = async function(url, filename) {
   return new Promise((resolve, reject) => {
     writer.on('finish', () => {
       console.timeEnd(label);
-      console.log(`Image created: ${path}`);
-      resolve(Path.join(IMAGE_PATH, `${filename}.${extension}`));
+      console.log(`Image downloaded: ${path}`);
+      resolve([
+        path,
+        virtualPath,
+      ]);
     });
     writer.on('error', (error) => {
       console.error(`Image download failure: ${label}`);
       reject(error);
     });
   });
+}
+
+const resizeImage = async function(filename) {
+  /*
+    All possible sizes:
+      Episode:
+        xs: 100% - 40px => 0 to 536px
+        sm: 540px - 40px = 500px
+        md: 720px / 3 - 40px => 200px
+        lg: 960px / 3 - 40px => 280px
+        xl: 1140px / 3 - 40px => 340px
+      Episode List:
+        xs: hidden
+        sm: hidden
+        md: (720px - 30px) / 6 - 35px => 80px
+        lg: (960px - 30px) / 6 - 35px => 120px
+        xl: (1140px - 30px) / 6 - 35px => 150px;
+    Generated sizes:
+      80px, 120px, 160px, 240px, 320px, 480px, 640px, 960px, 1280px
+  */
+  const directory = Path.dirname(filename);
+  const extension = Path.extname(filename);
+  const file = Path.basename(filename, extension);
+  const path = Path.resolve(IMAGE_PATH, Path.relative(IMAGE_DIRECTORY, directory));
+  const images = {};
+  const resizings = IMAGE_SIZES.map(async (size) => {
+    const resizing = Sharp(filename)
+      .resize({
+        width: size,
+        height: size,
+      });
+
+    await Promise.allSettled([
+      (async() => {
+        const jpegPath = Path.join(directory, `${file}@${size}w.jpg`);
+        const jpegVirtualPath = Path.join(path, `${file}@${size}w.jpg`);
+        await resizing
+          .jpeg({
+            quality: 90,
+            progressive: true,
+            chromaSubsampling: '4:4:4',
+          })
+          .toFile(jpegPath);
+        console.log(`Image resized: ${jpegPath}`);
+
+        images['image/jpeg'] = images['image/jpeg'] || {};
+        images['image/jpeg'][size] = jpegVirtualPath;
+      })(),
+
+      (async() => {
+        const pngPath = Path.join(directory, `${file}@${size}w.png`);
+        const pngVirtualPath = Path.join(path, `${file}@${size}w.png`);
+        await resizing
+          .png({
+            progressive: true,
+          })
+          .toFile(pngPath);
+        console.log(`Image resized: ${pngPath}`);
+
+        images['image/png'] = images['image/png'] || {};
+        images['image/png'][size] = pngVirtualPath;
+      })(),
+
+      (async() => {
+        const webpPath = Path.join(directory, `${file}@${size}w.webp`);
+        const webpVirtualPath = Path.join(path, `${file}@${size}w.webp`);
+        await resizing
+          .webp({
+            lossless: true,
+          })
+          .toFile(webpPath);
+        console.log(`Image resized: ${webpPath}`);
+
+        images['image/webp'] = images['image/webp'] || {};
+        images['image/webp'][size] = webpVirtualPath;
+      })(),
+    ]);
+  });
+
+  await Promise.all(resizings);
+
+  const results = IMAGE_TYPES.map((type) => {
+    return {
+      type,
+      sizes: IMAGE_SIZES.map((size) => {
+        return {
+          size,
+          image: images[type][size],
+        };
+      }),
+    };
+  });
+  return results;
 }
 
 module.exports = async function() {
@@ -145,16 +247,11 @@ module.exports = async function() {
       platform: 'Anchor',
     }];
 
-    anchorItem.itunes.images = [
-      anchorItem.itunes.image
-    ];
-
     if (typlogItem) {
       anchorItem.enclosures.push({
         ...typlogItem.enclosure,
         platform: 'Typlog',
       });
-      anchorItem.itunes.images.push(typlogItem.itunes.image);
     }
 
     if (ximalayaItem) {
@@ -163,22 +260,20 @@ module.exports = async function() {
         url: ximalayaToSecure(ximalayaItem.enclosure.url),
         platform: 'Ximalaya',
       });
-      anchorItem.itunes.images.push(ximalayaToSecure(ximalayaItem.itunes.image));
     }
 
     downloads.push((async() => {
-      anchorItem.itunes.image = await downloadImage(anchorItem.itunes.image, `episode_${anchorEpisode || index + 1}`);
-    })())
+      const [path, virtualPath] = await downloadImage(anchorItem.itunes.image, `episode_${anchorEpisode || index + 1}`);
+      anchorItem.itunes.image = virtualPath;
+      anchorItem.itunes.images = await resizeImage(path);
+    })());
   });
 
-  anchorFeed.itunes.images = [
-    anchorFeed.itunes.image,
-    typlogFeed.itunes.image,
-    ximalayaToSecure(ximalayaFeed.itunes.image),
-  ];
   downloads.push((async() => {
-    anchorFeed.itunes.image = await downloadImage(anchorFeed.itunes.image, 'feed');
-  })())
+    const [path, virtualPath] = await downloadImage(anchorFeed.itunes.image, 'feed');
+    anchorFeed.itunes.image = virtualPath;
+    anchorFeed.itunes.images = await resizeImage(path);
+  })());
 
   await Promise.allSettled(downloads);
 
